d881e83c9e3a1284ec53a013775499b80728b6da
no-la <nzyc0216@icloud.com>
2024-05-17 09:45:25 +0900

    add bot.py

---
+++ bot.py
@@ -0,0 +1,28 @@
+import json
+import discord
+
+# 自分のBotのアクセストークンに置き換えてください
+with open("secret.json", "r", encoding="utf-8") as f:
+    TOKEN = json.load(f)["discord"]["token"]
+
+# 接続に必要なオブジェクトを生成
+client = discord.Client(intents=discord.Intents.all())
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    # 起動したらターミナルにログイン通知が表示される
+    print('ログインしました')
+
+
+# メッセージ受信時に動作する処理
+@client.event
+async def on_message(message):
+    if message.author.bot:
+        return
+    # 「/neko」と発言したら「にゃーん」が返る処理
+    if message.content == '/neko':
+        await message.channel.send('にゃーん')
+
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
