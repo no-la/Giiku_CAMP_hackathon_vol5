91ee251811355e0087bbbf0a62658996c812e264
suupia <morita777k@icloud.com>
2024-05-18 17:29:10 +0900

    Fix out put message

--- cogs/numer_on_app_cog.py
+++ cogs/numer_on_app_cog.py
@@ -16,56 +16,42 @@ class NumerOnAppCog(commands.Cog):
     @commands.Cog.listener()
     async def on_ready(self):
         print("Cogs", "NumerOnAppCog is reloaded.")
-    
 
     @commands.command(name="nu")
     async def numer_on(self, ctx: commands.Context):
-        await ctx.send("NumerOnを開始します")
+        await ctx.send("NumerOnを開始します\n3桁の数字を当ててください")
         self.app_manager = numer_on_app.NumerOnAppManager(3)
         self.app_manager.start()
-        await ctx.send(f"origin = {self.app_manager.get_origin()}") 
+        print(f"origin = {self.app_manager.get_origin()}") 
         
-#     @commands.command(name="j")
-#     async def judge(self, ctx: commands.context, target: str = ""):
-#         # Precondition
-#         if target == "":
-#             await ctx.send(f"引数として数字を入力してください")
-#         if not target.isdigit():
-#             await ctx.send(f"数字を入力してください")
-#             return
-#     
-#         try:
-#             eat, bite = self.app_manager.judge(target)
-#             await ctx.send(f"Target: {target}, Eat: {eat}, Bite: {bite}")
-#         except ValueError as e:
-#             print(e)
-#         
-# 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         if message.author.bot:
             return
-        if message.content[0] ==  self.bot.command_prefix:
-            await message.channel.send("bot prefix")
+        if message.content[0] == self.bot.command_prefix:
+            print(f"bot prefix. message.content[0] {self.bot.command_prefix}")
             return
         
         if self.app_manager is not None:
-            await message.channel.send(f"state = {self.app_manager.state}")
-
+            print(f"state = {self.app_manager.state}")
+            
             if self.app_manager.state == numer_on_app.NumerOnAppState.INIT:
                 return
             if self.app_manager.state == numer_on_app.NumerOnAppState.PLAYING:
-                await message.channel.send(f"start judge")
+                print(f"start judge")
                 eat, bite = self.app_manager.judge(message.content)
-                await message.channel.send(f"Target: {message.content}, Eat: {eat}, Bite: {bite}")
+                print(f"Target: {message.content}, Eat: {eat}, Bite: {bite}")
+                await message.channel.send(f"Eat: {eat}, Bite: {bite}")
                 if eat == 3:
-                    await message.channel.send("Game is finished.")
+                    winner = message.author.nick
+                    if winner is None:
+                        winner = message.author.name
+                    await message.channel.send(f"クリア！勝者は{winner}です！\n正解するまでに: {self.app_manager.get_judge_count()}回かかりました")
                     self.app_manager.state = numer_on_app.NumerOnAppState.FINISHED
                 return
             else:
-                await message.channel.send("Game is finished.")
+                print(f"Game is finished.")
                 return
-        
 
 
 async def setup(bot):
