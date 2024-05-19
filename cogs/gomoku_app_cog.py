42de729363339c17196182ec7be9b054db22da9d
no-la <nzyc0216@icloud.com>
2024-05-19 14:59:39 +0900

    add turn judgement gomoku

--- cogs/gomoku_app_cog.py
+++ cogs/gomoku_app_cog.py
@@ -12,7 +12,7 @@ class GomokuAppCog(commands.Cog):
         super().__init__()
         self.bot = bot
         self.gomoku: gomoku_app.Gomoku = None
-        self.participant_ids = ["temp_participant_1", "temp_participant_2"]
+        self.participant_ids = None
     
     @commands.Cog.listener()
     async def on_ready(self):
@@ -20,30 +20,43 @@ class GomokuAppCog(commands.Cog):
     
     # ここに具体的な処理を書く
     @commands.command(name="go")
-    async def start(self, ctx: commands.Context):
+    async def ready(self, ctx: commands.Context):
         if self.gomoku is not None:
             await ctx.send(f"進行中のゲームがあります", embed=self.make_gomoku_embed(),
                            file=discord.File(gomoku_app.IMG_PATH))
             return
-        
-        self.gomoku = gomoku_app.Gomoku(19)
-        self.gomoku.make_board_img()
-        await ctx.send(embed=self.make_gomoku_embed(),
-                       file=discord.File(gomoku_app.IMG_PATH))
+        if self.participant_ids is None:
+            self.participant_ids = [ctx.author.id]
+            await ctx.send("もう一人の参加者も`/go`と送信してください")
+        else:
+            if ctx.author.id in self.participant_ids:
+                await ctx.send("参加済みです")
+                return
+            self.participant_ids.append(ctx.author.id)
+
+            await ctx.send(f"<@{self.participant_ids[0]}> が先手、<@{self.participant_ids[1]}> が後手です")
+            self.gomoku = gomoku_app.Gomoku(19)
+            self.gomoku.make_board_img()
+            await ctx.send(embed=self.make_gomoku_embed(),
+                        file=discord.File(gomoku_app.IMG_PATH))
     
     @commands.command(name="put")
     async def put_piece(self, ctx: commands.Context, x: int, y: int):
         if self.gomoku is None:
-            await ctx.send("`/gomoku`コマンドでゲームを始めてください")
+            await ctx.send("`/go`コマンドでゲームを始めてください")
             return
         
         try:
+            if ctx.author.id!=self.participant_ids[self.gomoku.get_turn()]:
+                await ctx.send(f"<@{self.participant_ids[self.gomoku.get_turn()]}> の手番です")
+                return
             self.gomoku.put_piece(y, x)
             self.gomoku.make_board_img()
             embed = self.make_gomoku_embed()
             if self.gomoku.winner is not None:
                 embed.color = 0xaa1111
                 self.gomoku = None
+                self.participant_ids = None
             await ctx.send(embed=embed, file=discord.File(gomoku_app.IMG_PATH))
         except (ValueError, IndexError) as e:
             await ctx.send(f"[ERROR]"+e.args[0])
@@ -51,7 +64,7 @@ class GomokuAppCog(commands.Cog):
     @commands.command(name="gofin")
     async def fin(self, ctx: commands.Context):
         self.gomoku = None
-        self.participant_ids.clear()
+        self.participant_ids = None
         
         await ctx.send("ゲームを終了しました")
         
